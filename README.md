bb5a2fa4edba833c4dad4cd5eed238e0d24610c0
franciane-brito <francianebrito99@gmail.com>
2024-03-18 09:34:02 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# 7-days-of-code-alura-react
+
+Projeto React.
+
+## Tecnologias:
+
+- JavaScript
+- React
+- React Hook Form
+- React Router
+- Tailwind
+- Firebase
+- ESLint
+- Vite
---
+++ src/pages/index.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import { Home } from "./Home";
+import { SignIn } from "./SignIn";
+import { SignUp } from "./SignUp";
+import { NotFound } from "./NotFound";
+
+export const Pages = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path='/sign-in' element={<SignIn />} />
+      <Route path='/sign-up' element={<SignUp />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </Router>
+);
